ee59c39c2de96777d874f900d08069d80103ec67
OuYangMinOa <wesley91345@gmail.com>
2024-03-11 04:12:40 +0800

    ake client workable

--- src/commands/client.rs
+++ src/commands/client.rs
@@ -1,10 +1,29 @@
 /// Talk to my local LLM sever
-
 use crate::Error;
+
 use reqwest;
+use serde::{Deserialize, Serialize};
+
+
+#[derive(Debug)]
+#[derive(Serialize, Deserialize)]
+struct PromptData{
+    ouput:String,
+    promptWord:String,
+    temperature:f32,
+    top_p:f32,
+
+}
+#[derive(Debug)]
+#[derive(Serialize, Deserialize)]
+struct PromptResult{
+    data:PromptData,
+    status:String,
+}
+
 
 #[allow(unused)]
-pub async fn prompt_msg(msg:&String) -> Result<(),Error>{
+pub async fn prompt_msg(msg:&String) -> Result<String,Error>{
     let json = &serde_json::json!({
         "promptWord":msg,
         "top_p":0.2,
@@ -12,9 +31,13 @@ pub async fn prompt_msg(msg:&String) -> Result<(),Error>{
     });
 
     let client = reqwest::Client::new();
-    let _: reqwest::Response = client.post("http://127.0.0.1:8088/prompt")
+    let res= client.post("http://127.0.0.1:8088/prompt")
         .json(json)
         .send()
+        .await?
+        .text()
         .await?;
-    Ok(())
+    let res_json:PromptResult = serde_json::from_str(res.as_str()).unwrap();
+
+    Ok(res_json.data.ouput)
 }
